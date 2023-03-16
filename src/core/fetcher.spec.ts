2090c06f3d5d31d11e6050166f5e66531ec0a25e
falsepopsky <falsepopsky@protonmail.com>
2023-03-15 22:00:00 -0300

    feat: add fetcher test and CI

---
+++ src/core/fetcher.spec.ts
@@ -0,0 +1,15 @@
+import { describe, expect, test } from '@jest/globals';
+import { fetcher } from './fetcher.js';
+
+describe('fetcher function', () => {
+  test('throws an error when response.ok is not true', async () => {
+    const urlStatusNotOK = 'https://httpstat.us/404';
+    await expect(fetcher(urlStatusNotOK)).rejects.toThrow(Error);
+  });
+
+  test('returns text when response.ok is true', async () => {
+    const urlStatusOK = 'https://httpstat.us/200';
+    const text = await fetcher(urlStatusOK);
+    expect(text).toBe('200 OK');
+  });
+});
